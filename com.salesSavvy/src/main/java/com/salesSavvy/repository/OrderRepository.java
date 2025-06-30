69f0556a8ec2263d4e5cd68604a209aaf797e0b7
Nishant_Totar <70787217+nstotar@users.noreply.github.com>
2025-06-30 22:08:06 +0530

    Add full project files

---
+++ com.salesSavvy/src/main/java/com/salesSavvy/repository/OrderRepository.java
@@ -0,0 +1,4 @@
+package com.salesSavvy.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.salesSavvy.entity.Orders;
+public interface OrderRepository extends JpaRepository<Orders,String>{}
